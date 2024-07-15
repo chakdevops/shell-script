4b0e987eac5b893e990e9b6a32fa95fa3b10d4a9
chakdevops <arunnine.ch@gmail.com>
2024-07-15 11:36:16 +0530

    Shell Scripts

--- arg1.sh
+++ arg1.sh
@@ -3,4 +3,8 @@
 USERNAME=$1
 PASSWORD=$2
 
-echo "Username is: $USERNAME, Password is: $PASSWORD"
+echo read -s  "Enter your Username: "
+echo read -p  "Enter your Password: "
+
+
+# echo "Username is: $USERNAME, Password is: $PASSWORD"
