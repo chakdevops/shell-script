81e92f30bfa46ab12f0633b0afb765be74c6a2c0
chakdevops <arunnine.ch@gmail.com>
2024-07-19 12:42:36 +0530

    Shell Scripts

--- arg.sh
+++ arg.sh
@@ -7,3 +7,7 @@ echo "$PERSON1: Hello $PERSON2, Good morning!"
 echo "$PERSON2: Hi $PERSON1, Very Good morning"
 echo "$PERSON1 How are you $PERSON2?"
 echo "$PERSON2 I am good $PERSON1, How abt you?"
+
+echo "args passed:: $#"
+echo "Script name:: $0"
+echo "all arguments:: $@"
